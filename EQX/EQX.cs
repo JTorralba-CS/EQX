5deb95e7bb1b2dd6cdd6c9eb91cc01b2794fe29c
Julius Torralba <Julius.Torralba@GMail.com>
2024-08-09 22:38:37 -0600

    Implement JSON processing.

--- EQX/EQX.cs
+++ EQX/EQX.cs
@@ -5,6 +5,10 @@ using System.Runtime.InteropServices;
 using System.Xml;
 
 using RGiesecke.DllExport;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace EQX
 {
@@ -18,6 +22,8 @@ namespace EQX
         [DllExport("VendorName", CallingConvention.Cdecl)]
         public static int VendorName([MarshalAs(UnmanagedType.LPWStr)] out string VendorName)
         {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
             VendorName = string.Format("EQX", (object) Environment.NewLine);
 
             return 0;
@@ -47,12 +53,24 @@ namespace EQX
                 WriteToDebug(string.Format("_URL: {0}", (object) _URL));
                 WriteToDebug(string.Format("_U: {0}", (object) _U));
                 WriteToDebug(string.Format("_P: {0}", (object) _P));
+                WriteToDebug("");
+
+                string AuthenticateResponse = @"{'success':true,'reason':0}";
+
+                AuthenticateResponse_Root AuthenticateResponse_DeJSON = JsonConvert.DeserializeObject<AuthenticateResponse_Root>(AuthenticateResponse);
+
+                WriteToDebug("Success = " + AuthenticateResponse_DeJSON.Success.ToString());
+                WriteToDebug("Reason = " + AuthenticateResponse_DeJSON.Reason.ToString());
 
                 Code = 0;
             }
             catch (Exception ex)
             {
                 WriteToDebug(string.Format("Authenticate Error: {0}", (object) ex.Message));
+            }
+            finally
+            {
+                WriteToDebug("");
             }
 
             return Code;
@@ -67,29 +85,53 @@ namespace EQX
 
             try
             {
-                FieldData = string.Format("<search_fields>{0}", (object) Environment.NewLine);
-
-                FieldData += string.Format("<field>{0}<name>destinationdevice</name>{1}<heading>Destination Device</heading>{2}</field>{3}", (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine);
-                FieldData += string.Format("<field>{0}<name>destinationuser</name>{1}<heading>Destination User</heading>{2}</field>{3}", (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine);
-                FieldData += string.Format("<field>{0}<name>device</name>{1}<heading>Device</heading>{2}</field>{3}", (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine);
-                FieldData += string.Format("<field>{0}<name>durationMax</name>{1}<heading>Duration Maximum</heading>{2}</field>{3}", (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine);
-                FieldData += string.Format("<field>{0}<name>durationMin</name>{1}<heading>Duration Minimum</heading>{2}</field>{3}", (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine);
-                //FieldData += string.Format("<field>{0}<name>endtime</name>{1}<heading>End Time</heading>{2}</field>{3}", (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine);
-                FieldData += string.Format("<field>{0}<name>sourcedevice</name>{1}<heading>Source Device</heading>{2}</field>{3}", (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine);
-                FieldData += string.Format("<field>{0}<name>sourceuser</name>{1}<heading>Source User</heading>{2}</field>{3}", (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine);
-                //FieldData += string.Format("<field>{0}<name>starttime</name>{1}<heading>Start Time</heading>{2}</field>{3}", (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine);
-                FieldData += string.Format("<field>{0}<name>user</name>{1}<heading>User</heading>{2}</field>{3}", (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine, (object) Environment.NewLine);
-
-                FieldData += string.Format("</search_fields>{0}", (object) Environment.NewLine);
-
                 WriteToDebug("SEARCHFIELDS --------------------------------------------------");
-                WriteToDebug(string.Format("FieldData: {0}", (object) FieldData));
+                string SearchFieldsResponse = @"{'success':true,'parameters': [{'description':'The name or address of the callee','displayName':'Destination Device','name':'destinationdevice'},{'description':'The account/name of the callee','displayName':'Destination User','name':'destinationuser'},{'description':'The name or address of a device','displayName':'Device','name':'device'},{'description':'The maximum duration of a call in milliseconds','displayName':'Maximum Duration','name':'durationMax'},{'description':'The minimum duration of a call in milliseconds','displayName':'Minimum Duration','name':'durationMin'},{'description':'The end of a time range','displayName':'End Time','name':'endtime'},{'description':'The name or address of the caller','displayName':'Source Device','name':'sourcedevice'},{'description':'The account/name of the caller','displayName':'Source User','name':'sourceuser'},{'description':'The beginning of a time range','displayName':'Start Time','name':'starttime'},{'description':'The account name of a user','displayName':'User','name':'user'}]}";
+
+                SearchFieldsResponse_Root SearchFieldsResponse_DeJSON = JsonConvert.DeserializeObject<SearchFieldsResponse_Root>(SearchFieldsResponse);
+
+                WriteToDebug("Success = " + SearchFieldsResponse_DeJSON.Success.ToString());
+
+                var Item = SearchFieldsResponse_DeJSON.Parameters.Single(X => X.Name == "device");
+                SearchFieldsResponse_DeJSON.Parameters.Remove(Item);
+
+                Item = SearchFieldsResponse_DeJSON.Parameters.Single(X => X.Name == "durationMin");
+                SearchFieldsResponse_DeJSON.Parameters.Remove(Item);
+
+                Item = SearchFieldsResponse_DeJSON.Parameters.Single(X => X.Name == "endtime");
+                SearchFieldsResponse_DeJSON.Parameters.Remove(Item);
+
+                Item = SearchFieldsResponse_DeJSON.Parameters.Single(X => X.Name == "starttime");
+                SearchFieldsResponse_DeJSON.Parameters.Remove(Item);
+
+                Item = SearchFieldsResponse_DeJSON.Parameters.Single(X => X.Name == "user");
+                SearchFieldsResponse_DeJSON.Parameters.Remove(Item);
+
+                foreach (SearchFieldsResponse_Parameter Parameter in SearchFieldsResponse_DeJSON.Parameters)
+                {
+                    WriteToDebug(Parameter.DisplayName + " : " + Parameter.Name);
+                }
+
+                WriteToDebug("");
+
+                XElement XML = new XElement("search_fields",
+                    from Parameter in SearchFieldsResponse_DeJSON.Parameters
+                    select new XElement("field", new XElement("heading", Parameter.DisplayName), new XElement("name", Parameter.Name))
+                    );
+
+                FieldData = XML.ToString(SaveOptions.DisableFormatting);
+
+                WriteToDebug(FieldData);
 
                 Code = 0;
             }
             catch (Exception ex)
             {
                 WriteToDebug(string.Format("SearchFields Error: {0}", (object) ex.Message));
+            }
+            finally
+            {
+                WriteToDebug("");
             }
 
             return Code;
@@ -105,7 +147,7 @@ namespace EQX
 
             try
             {
-                WriteToDebug("SEARCHAUDIO --------------------------------------------------");
+                WriteToDebug("SEARCHAUDIO ---------------------------------------------------");
 
                 XmlDocument XMLDocument = new XmlDocument();
                 XMLDocument.LoadXml(SearchData);
@@ -137,51 +179,41 @@ namespace EQX
                     }
                 }
 
-                string dstAddr = "1128";
-                string dstName = "CSPD Phone";
-                string endTime = "2024-06-25T15:58:57.303";
-                string id = "2024062509570071852";
-                string len = "116440";
-                string srcAddr = "7195110413";
-                string srcName = "";
-                string staTime = "2024-06-25T15:57:00.863";
+                WriteToDebug("");
 
-                SearchResults = string.Format("<search_results>{0}", (object) Environment.NewLine);
+                string SearchAudioResponse = @"{'success':true,'results':[{'dstAddr':'1128','dstName':'CSPD Phone','endTime':'2024-08-08 15:15:05.170','id':'2024080809124036330','len':145040,'srcAddr':'7197612536','srcName':'','staTime':'2024-08-08 15:12:40.130'},{'dstAddr':'1128','dstName':'CSPD Phone','endTime':'2024-08-08 15:16:30.450','id':'2024080809151060403','len':80080,'srcAddr':'7195114554','srcName':'','staTime':'2024-08-08 15:15:10.370'},{'dstAddr':'1128','dstName':'CSPD Phone','endTime':'2024-08-08 15:21:37.120','id':'2024080809175215032','len':224200,'srcAddr':'8644368579','srcName':'','staTime':'2024-08-08 15:17:52.920'},{'dstAddr':'1128','dstName':'CSPD Phone','endTime':'2024-08-08 15:33:27.820','id':'2024080809265976858','len':388280,'srcAddr':'7193132063','srcName':'','staTime':'2024-08-08 15:26:59.540'},{'dstAddr':'1128','dstName':'CSPD Phone','endTime':'2024-08-08 15:43:28.767','id':'2024080809403611357','len':171880,'srcAddr':'7196358306','srcName':'','staTime':'2024-08-08 15:40:36.887'}],'resultsTotal':5}";
 
-                string destinationdevice = dstAddr;
-                string destinationuser = dstName;
-                string device = "_device";
-                string durationMax = "_durationMax";
-                string durationMin = "_durationMin";
-                string sourcedevice = srcAddr;
-                string sourceuser = "_sourceuser";
-                string user = "_user";
+                SearchAudioResponse_Root SearchAudioResponse_DeJSON = JsonConvert.DeserializeObject<SearchAudioResponse_Root>(SearchAudioResponse);
 
-                string AudioData = "";
+                WriteToDebug("Success = " + SearchAudioResponse_DeJSON.Success.ToString());
 
-                AudioData = string.Format("<audio>{0}", (object) Environment.NewLine);
+                foreach (SearchAudioResponse_Result Result in SearchAudioResponse_DeJSON.Results)
+                {
+                    WriteToDebug(Result.ID + " " + Result.StaTime + " " + Result.EndTime + " " + Result.DstName + " " + Result.DstAddr + " " + Result.SrcAddr);
+                }
 
-                AudioData += string.Format("<start_time>{0}</start_time>{1}", (object) staTime, (object) Environment.NewLine);
-                AudioData += string.Format("<end_time>{0}</end_time>{1}", (object) endTime, (object) Environment.NewLine);
+                WriteToDebug("ResultsTotal = " + SearchAudioResponse_DeJSON.ResultsTotal.ToString());
 
-                AudioData += string.Format("<destinationdevice>{0}</destinationdevice>{1}", (object) destinationdevice, (object) Environment.NewLine);
-                AudioData += string.Format("<destinationuser>{0}</destinationuser>{1}", (object) destinationuser, (object) Environment.NewLine);
-                AudioData += string.Format("<device>{0}</device>{1}", (object) device, (object) Environment.NewLine);
-                AudioData += string.Format("<durationMax>{0}</durationMax>{1}", (object) durationMax, (object) Environment.NewLine);
-                AudioData += string.Format("<durationMin>{0}</durationMin>{1}", (object) durationMin, (object) Environment.NewLine);              
-                AudioData += string.Format("<sourcedevice>{0}</sourcedevice>{1}", (object) sourcedevice, (object) Environment.NewLine);
-                AudioData += string.Format("<sourceuser>{0}</sourceuser>{1}", (object) sourceuser, (object) Environment.NewLine);
-                AudioData += string.Format("<user>{0}</user>{1}", (object) user, (object) Environment.NewLine);
+                WriteToDebug("");
 
-                AudioData += string.Format("<id>{0}</id>{1}", (object) id, (object) Environment.NewLine);
+                XElement XML = new XElement("search_results",
+                    from Result in SearchAudioResponse_DeJSON.Results
+                    select new XElement("audio",
+                        new XElement("start_time", Result.StaTime),
+                        new XElement("end_time", Result.EndTime),
+                        new XElement("id", Result.ID),
+                        new XElement("search_fields",
+                            new XElement("field", new XElement("name", "destinationdevice"), new XElement("value", Result.DstAddr)),
+                            new XElement("field", new XElement("name", "destinationuser"), new XElement("value", Result.DstName)),
+                            new XElement("field", new XElement("name", "durationmax"), new XElement("value", Duration(Result.Len))),
+                            new XElement("field", new XElement("name", "sourcedevice"), new XElement("value", Result.SrcAddr)),
+                            new XElement("field", new XElement("name", "sourceuser"), new XElement("value", Result.SrcName))
+                        )
+                    )
+                );
 
-                AudioData += string.Format("</audio>{0}", (object) Environment.NewLine);
-
-                SearchResults += AudioData;
-
-                SearchResults += string.Format("</search_results>{0}", (object) Environment.NewLine);
-
-                WriteToDebug(string.Format("SearchResults: {0}", (object) SearchResults));
+                SearchResults = XML.ToString(SaveOptions.DisableFormatting);
+                WriteToDebug(SearchResults);
 
                 Code = 1;
             }
@@ -189,6 +221,11 @@ namespace EQX
             {
                 WriteToDebug(string.Format("SearchAudio Error: {0}", (object) ex.Message));
             }
+            finally
+            {
+                WriteToDebug("");
+            }
+
             return Code;
         }
 
@@ -216,9 +253,17 @@ namespace EQX
             WriteToDebug(string.Format("StartPos: {0}", (object) StartPos));
             WriteToDebug(string.Format("WinHandle: {0}", (object) WinHandle));
 
-            URL = _URL.ToUpper().Contains("RECORDER02") ? string.Format("{0}/ViewPoint/getfile.ashx?fileid={1}{2}", (object) _URL, (object) ID, (object) Environment.NewLine) : string.Format("{0}\\{1}{2}", (object) _URL, (object) AudioFile, (object)Environment.NewLine);
+            WriteToDebug("");
 
-            WriteToDebug(string.Format("URL: {0}", (object) URL));
+            string PlayAudioResponse = @"{'success':true,'size':279365,'url':'http://192.168.200.240/ViewPoint/getfile.ashx?fileid=" + @ID + "'}";
+
+            PlayAudioResponse_Root PlayAudioResponse_DeJSON = JsonConvert.DeserializeObject<PlayAudioResponse_Root>(PlayAudioResponse);
+
+            WriteToDebug("Success = " + PlayAudioResponse_DeJSON.Success.ToString());
+            WriteToDebug("Size = " + PlayAudioResponse_DeJSON.Size.ToString());
+
+            URL = PlayAudioResponse_DeJSON.URL;
+            WriteToDebug("URL = " + URL);
 
             return 0;
         }
@@ -243,24 +288,6 @@ namespace EQX
 
         [DllExport("FreeMemory", CallingConvention.Cdecl)]
         public static int FreeMemory([MarshalAs(UnmanagedType.LPWStr)] string MemoryToFree) => 0;
-
-        private static Assembly CurrentDomain_AssemblyResolve(object Sender, ResolveEventArgs args)
-        {
-            if (args.Name.Contains("Newtonsoft.Json"))
-            {
-                foreach (string ManifestResourceName in Assembly.GetExecutingAssembly().GetManifestResourceNames())
-                {
-                    if (ManifestResourceName.EndsWith("Newtonsoft.Json.dll"))
-                    {
-                        Stream ManifestResourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(ManifestResourceName);
-                        byte[] NumArray = new byte[ManifestResourceStream.Length];
-                        ManifestResourceStream.Read(NumArray, 0, NumArray.Length);
-                        return Assembly.Load(NumArray);
-                    }
-                }
-            }
-            return (Assembly)null;
-        }
 
         private static byte[] StreamToBytes(Stream Input)
         {
@@ -289,5 +316,77 @@ namespace EQX
                 streamWriter.Close();
             }
         }
+
+        public static void ListEmbeddedResourceNames()
+        {
+            WriteToDebug("RESOURCE ---------------------------------------------------");
+
+            foreach (var Resource in Assembly.GetExecutingAssembly().GetManifestResourceNames())
+                WriteToDebug("Resource: " + Resource);
+
+            WriteToDebug("");
+
+        }
+
+        public static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("EQX.EmbeddedAssemblies.Newtonsoft.Json.dll"))
+            {
+                var assemblyData = new Byte[stream.Length];
+                stream.Read(assemblyData, 0, assemblyData.Length);
+                return Assembly.Load(assemblyData);
+            }
+        }
+
+        public static string Duration(int MilliSeconds)
+        {
+            TimeSpan Time = TimeSpan.FromMilliseconds(MilliSeconds);
+            return Time.Hours.ToString("D2") + ":" + Time.Minutes.ToString("D2") + ":" + Time.Seconds.ToString("D2");
+        }
+    }
+
+    public class AuthenticateResponse_Root
+    {
+        public bool Success { get; set; }
+        public int Reason { get; set; }
+    }
+
+    public class SearchFieldsResponse_Root
+    {
+        public bool Success { get; set; }
+        public List<SearchFieldsResponse_Parameter> Parameters { get; set; }
+    }
+
+    public class SearchFieldsResponse_Parameter
+    {
+        public string Description { get; set; }
+        public string DisplayName { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class SearchAudioResponse_Root
+    {
+        public bool Success { get; set; }
+        public List<SearchAudioResponse_Result> Results { get; set; }
+        public int ResultsTotal { get; set; }
+    }
+
+    public class SearchAudioResponse_Result
+    {
+        public string DstAddr { get; set; }
+        public string DstName { get; set; }
+        public string EndTime { get; set; }
+        public string ID { get; set; }
+        public int Len { get; set; }
+        public string SrcAddr { get; set; }
+        public string SrcName { get; set; }
+        public string StaTime { get; set; }
+    }
+
+    public class PlayAudioResponse_Root
+    {
+        public bool Success { get; set; }
+        public int Size { get; set; }
+        public string URL { get; set; }
     }
 }
